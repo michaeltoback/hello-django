c95071cdcb0f57fc5c8e4d98282f1b475b04c448
Michael Toback <michael.toback@gmail.com>
2014-10-02 20:54:47 -0700

    Added REST API via tastypie for historical data

--- hellodjango/hellodjango/urls.py
+++ hellodjango/hellodjango/urls.py
@@ -2,7 +2,9 @@
 # -*- coding: utf-8 -*-
 from django.conf.urls import patterns, include, url
 from django.contrib import admin
+from hello.api import LegislatorResource
 
+legislator_resource = LegislatorResource()
 
 urlpatterns = patterns('',
     # Examples:
@@ -11,4 +13,5 @@ urlpatterns = patterns('',
     url(r'^$', 'hello.views.home', name='home'),
     url(r'^admin/', include(admin.site.urls)),
     url(r'^ajax/',  'hello.views.ajax', name='ajax'),
+    url(r'^api/', include(legislator_resource.urls)),
 )
