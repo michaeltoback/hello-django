0ce61d57f077c6b2c0e2acbbbf011fac8c6d4dc1
Michael Toback <michael.toback@gmail.com>
2014-10-03 20:48:44 -0700

    added queries for term and legislator including compares on dates and names

--- hellodjango/hello/api.py
+++ hellodjango/hello/api.py
@@ -1,11 +1,28 @@
 from tastypie.resources import ModelResource, ALL
+import tastypie.fields
 from hello.models import Legislator, Terms
+
 
 class LegislatorResource(ModelResource):
     class Meta:
         queryset = Legislator.objects.all()
+        allowed_methods = ['get']
         resource_name = 'legislator'
         filtering = {
                      'first_name': ALL,
                      'last_name':ALL,
                      }
+
+class TermsResource(ModelResource):
+    legislator = tastypie.fields.ToOneField(LegislatorResource, 'bioguide_id')
+    class Meta:
+        queryset = Terms.objects.all()
+        resource_name = 'terms'
+        allowed_methods = ['get']
+        filtering = {
+                     'terms_start': ['exact', 'range', 'gt', 'gte', 'lt', 'lte'],
+                     'terms_end':['exact', 'range', 'gt', 'gte', 'lt', 'lte'],
+                     'terms_state':ALL,
+                     }
+
+
--- hellodjango/hellodjango/urls.py
+++ hellodjango/hellodjango/urls.py
@@ -2,9 +2,10 @@
 # -*- coding: utf-8 -*-
 from django.conf.urls import patterns, include, url
 from django.contrib import admin
-from hello.api import LegislatorResource
+from hello.api import LegislatorResource, TermsResource
 
 legislator_resource = LegislatorResource()
+terms_resource = TermsResource()
 
 urlpatterns = patterns('',
     # Examples:
@@ -13,5 +14,6 @@ urlpatterns = patterns('',
     url(r'^$', 'hello.views.home', name='home'),
     url(r'^admin/', include(admin.site.urls)),
     url(r'^ajax/',  'hello.views.ajax', name='ajax'),
-    url(r'^api/', include(legislator_resource.urls)),
+    url(r'^api1/', include(terms_resource.urls,)),
+    url(r'^api2/', include(legislator_resource.urls,)),
 )
